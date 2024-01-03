32705ec882a149cb84aaf6d3dd96a3034f133fde
Rohan Gope <rg2512@it.jgec.ac.in>
2024-01-03 19:23:20 +0530

    Refactor variable names for clarity

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+    constructor(data, message="Success", statusCode) {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode < 400 ? true : false;
+    }
+}
