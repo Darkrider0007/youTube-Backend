d8f4aaba193da9f0577858524f74a61478075253
Rohan Gope <rg2512@it.jgec.ac.in>
2024-01-13 17:14:45 +0530

    Add logout functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -151,6 +151,9 @@ const loginUser = asyncHandler(async (req,res) => {
 })
 
 const logoutUser = asyncHandler(async(req, res) => {
+  // clear the refresh token from the user document
+  // clear the refresh token from the cookie
+  // return response
   await User.findByIdAndUpdate(
       req.user._id,
       {
