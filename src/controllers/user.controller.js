b4d1aad5be97ad5edd0d380c3394559bb0983427
Rohan Gope <rg2512@it.jgec.ac.in>
2024-01-28 12:54:32 +0530

    Add watchHistory endpoint to user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -468,6 +468,57 @@ const getUserChannelProfile = asyncHandler(async(req,res) => {
   .json(new ApiResponse(200, channel[0], "Channel fetched successfully"))
 })
 
+const watchHistory = asyncHandler(async(req,res) => {
+  const user = await User.aggregate([
+    {
+      $match:{
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup:{
+        from: "videos",
+        localField:"watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline:[
+          {
+            $lookup:{
+              from:"users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline:[
+                {
+                  $project:{
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields:{
+              owner:{
+                $first: "$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return res
+  .status(200)
+  .json(new ApiResponse(
+    200, user[0]?.watchHistory || [],
+    "Watch history fetched successfully"
+  ))
+})
+
 export { 
   registerUser,
   loginUser,
@@ -478,5 +529,6 @@ export {
   updateUserDetails,
   updateUserAvatar,
   updateUserCoverImage,
-  getUserChannelProfile
+  getUserChannelProfile,
+  watchHistory
 };
