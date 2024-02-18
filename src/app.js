8a1b8c9ec5b1e9c05a43b84d72c0b737a0dc1ee0
Rohan Gope <rg2512@it.jgec.ac.in>
2024-02-18 16:38:54 +0530

    Add dashboard route to app.js

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import playlistRouter from './routes/playlist.routes.js';
 import commentRouter from './routes/comment.routes.js';
 import likeRouter from './routes/like.routes.js';
+import dashboardRouter from './routes/dashboard.routes.js';
 
 //router declaration
 app.use('/api/v1/users', userRouter);
@@ -34,6 +35,7 @@ app.use('/api/v1/subscription', subscriptionRouter);
 app.use('/api/v1/playlist', playlistRouter);
 app.use('/api/v1/comment', commentRouter);
 app.use('/api/v1/like', likeRouter);
+app.use('/api/v1/dashboard', dashboardRouter);
 
 // http://localhost:8000/api/v1/users/register
 // http://localhost:8000/api/v1/healthCheck/
